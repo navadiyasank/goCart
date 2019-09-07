87e4a49200fc4fc40d9386981865b847f3f6cacc
sanket navadiya <sanknavadiya162@gmail.com>
2019-09-07 21:44:37 +0530

    Js changed in go_cart snippet

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -17,42 +17,80 @@ class Shop < ActiveRecord::Base
   	ShopifyAPI::Base.site = "https://#{ShopifyApp.configuration.api_key}:#{self.shopify_token}@#{self.shopify_domain}/admin/"
     ShopifyAPI::Base.api_version = ShopifyApp.configuration.api_version
     @theme = ShopifyAPI::Theme.find(:all).where(role: 'main').first
-    @asset = ShopifyAPI::Asset.create(key: 'snippets/go-cart.liquid', value: "
-      <script>
-        window.goCart = {
-          shopify_domain: '{{shop.permanent_domain}}',
-          app_url: '#{ENV['DOMAIN']}',
-        }
-        $.ajax({
-          type:'GET',
-          url: window.goCart.app_url+'/frontend/get_gocart_details',
-          data : {shopify_domain : window.goCart.shopify_domain},
-          crossDomain: true,
-          success:function(data){
-            var goCart_is_active = data.is_active;
-            var icon_color = data.color
-            var icon_shape = data.shape
-            var icon_type = data.icon_type
-            console.log('response==',data);
-            if(goCart_is_active){
-              $( \"a[href='/cart']\" ).each(function( index ) {
-                height = $( this ).height();
-                b_class=$( this ).attr('class');
-                {% if cart.item_count > 0 %}
-                  cart_image = {{ cart.items.last.image | json }}
-                {% endif %}
-                if(icon_type == 'cart'){
-                  str_svg = \"<svg fill='\"+icon_color+\"' version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='79 -79 670 670' style='enable-background:new 79 -79 670 670;' xml:space='preserve'><g id='XMLID_85_'><g id='XMLID_86_'><path id='XMLID_87_' class='st0' d='M724,171c-4.6-5.9-11.6-9.3-19.1-9.3h-465L218,70.3c-2.6-10.9-12.4-18.6-23.6-18.6h-80.1 C100.9,51.7,90,62.6,90,76s10.9,24.3,24.3,24.3h61l78.9,330.2c2.6,10.9,12.4,18.6,23.6,18.6h368c11.2,0,20.9-7.6,23.6-18.4 l59.1-238.8C730.2,184.6,728.6,176.9,724,171z M626.8,400.5H296.9l-45.5-190.2h422.3L626.8,400.5z'/></g></g><circle id='XMLID_84_' class='st0' cx='331.6' cy='512.9' r='48'/><circle id='XMLID_83_' class='st0' cx='596' cy='512.9' r='48'/></svg>\"
-                }else{
-                  str_svg = \"<svg fill='\"+icon_color+\"' stroke='\"+icon_color+\"' stroke-width='20' stroke-miterlimit='10' version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 600 600' style='enable-background:new 0 0 600 600;' xml:space='preserve'> <path id='XMLID_41_' class='st0' d='M490.2,131.6h-76.9v2.5c0-59.8-48.5-108.2-108.2-108.2c-59.8,0-108.2,48.5-108.2,108.2v-2.5 h-83.3c-6.9,0-12.4,5.6-12.5,12.5v363.9c0,34.4,27.9,62.3,62.3,62.3h277c34.4,0,62.3-27.9,62.3-62.3V144.1 C502.7,137.2,497.1,131.6,490.2,131.6z M221.7,133.9c0-46,37.3-83.3,83.3-83.3c46,0,83.3,37.3,83.3,83.3v-2.5H221.7V133.9z M477.7,507.9c-0.1,20.6-16.8,37.3-37.4,37.4h-277c-20.6-0.1-37.3-16.8-37.4-37.4V156.5h70.8v50.7c0,6.9,5.6,12.5,12.5,12.5 c6.9,0,12.5-5.6,12.5-12.5v-50.7h166.7v50.7c0,6.9,5.6,12.5,12.5,12.5c6.9,0,12.5-5.6,12.5-12.5v-50.7h64.5V507.9z'/> </svg>\"
-                }
-              
-              $( this ).replaceWith(\"<div class='go-cart-icon go-\"+icon_type+\" \"+icon_shape+\"'><a href='/cart' class='\"+b_class+\"'>\"+str_svg+\"{% if cart.item_count > 0 %}<img class='go-cart-item' src='\"+cart_image+\"' alt='cart' style='border-color:\"+icon_color+\";'><div id='CartCount' class='site-header__cart-count'><span>{{ cart.item_count }}</span><span class='icon__fallback-text medium-up--hide'>{{ 'layout.cart.items_count' | t: count: cart.item_count }}</span></div>{% endif %}</div></a>\")
-              });
-            }
+    @asset = ShopifyAPI::Asset.create(key: 'snippets/go-cart.liquid', value: "<script>
+      function start(){
+        window.loadScript = function(url, callback) {
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          // If the browser is Internet Explorer
+          if (script.readyState){
+            script.onreadystatechange = function() {
+              if (script.readyState == 'loaded' || script.readyState == 'complete') {
+                script.onreadystatechange = null;
+                callback();
+              }
+            };
+            // For any other browser
+          } else {
+            script.onload = function() {
+              callback();
+            };
           }
+          script.src = url;
+          document.getElementsByTagName('head')[0].appendChild(script);
+        };
+
+        window.goCartStart = function($) {
+          window.goCart = {
+            shopify_domain: '{{shop.permanent_domain}}',
+            app_url: '#{ENV['DOMAIN']}',
+          }
+          $.ajax({
+            type:'GET',
+            url: window.goCart.app_url+'/frontend/get_gocart_details',
+            data : {shopify_domain : window.goCart.shopify_domain},
+            crossDomain: true,
+            success:function(data){
+              var goCart_is_active = data.is_active;
+              var icon_color = data.color
+              var icon_shape = data.shape
+              var icon_type = data.icon_type
+              console.log('response==',data);
+              if(goCart_is_active){
+                $( \"a[href='/cart']\" ).each(function( index ) {
+                  height = $( this ).height();
+                  b_class=$( this ).attr('class');
+                  {% if cart.item_count > 0 %}
+                    cart_image = {{ cart.items.last.image | json }}
+                  {% endif %}
+                  if(icon_type == 'cart'){
+                    str_svg = \"<svg fill='\"+icon_color+\"' version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='79 -79 670 670' style='enable-background:new 79 -79 670 670;' xml:space='preserve'><g id='XMLID_85_'><g id='XMLID_86_'><path id='XMLID_87_' class='st0' d='M724,171c-4.6-5.9-11.6-9.3-19.1-9.3h-465L218,70.3c-2.6-10.9-12.4-18.6-23.6-18.6h-80.1 C100.9,51.7,90,62.6,90,76s10.9,24.3,24.3,24.3h61l78.9,330.2c2.6,10.9,12.4,18.6,23.6,18.6h368c11.2,0,20.9-7.6,23.6-18.4 l59.1-238.8C730.2,184.6,728.6,176.9,724,171z M626.8,400.5H296.9l-45.5-190.2h422.3L626.8,400.5z'/></g></g><circle id='XMLID_84_' class='st0' cx='331.6' cy='512.9' r='48'/><circle id='XMLID_83_' class='st0' cx='596' cy='512.9' r='48'/></svg>\"
+                  }else{
+                    str_svg = \"<svg fill='\"+icon_color+\"' stroke='\"+icon_color+\"' stroke-width='20' stroke-miterlimit='10' version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 600 600' style='enable-background:new 0 0 600 600;' xml:space='preserve'> <path id='XMLID_41_' class='st0' d='M490.2,131.6h-76.9v2.5c0-59.8-48.5-108.2-108.2-108.2c-59.8,0-108.2,48.5-108.2,108.2v-2.5 h-83.3c-6.9,0-12.4,5.6-12.5,12.5v363.9c0,34.4,27.9,62.3,62.3,62.3h277c34.4,0,62.3-27.9,62.3-62.3V144.1 C502.7,137.2,497.1,131.6,490.2,131.6z M221.7,133.9c0-46,37.3-83.3,83.3-83.3c46,0,83.3,37.3,83.3,83.3v-2.5H221.7V133.9z M477.7,507.9c-0.1,20.6-16.8,37.3-37.4,37.4h-277c-20.6-0.1-37.3-16.8-37.4-37.4V156.5h70.8v50.7c0,6.9,5.6,12.5,12.5,12.5 c6.9,0,12.5-5.6,12.5-12.5v-50.7h166.7v50.7c0,6.9,5.6,12.5,12.5,12.5c6.9,0,12.5-5.6,12.5-12.5v-50.7h64.5V507.9z'/> </svg>\"
+                  }
+                
+                $( this ).replaceWith(\"<div class='go-cart-icon go-\"+icon_type+\" \"+icon_shape+\"'><a href='/cart' class='\"+b_class+\"'>\"+str_svg+\"{% if cart.item_count > 0 %}<img class='go-cart-item' src='\"+cart_image+\"' alt='cart' style='border-color:\"+icon_color+\";'><div id='CartCount' class='site-header__cart-count'><span>{{ cart.item_count }}</span><span class='icon__fallback-text medium-up--hide'>{{ 'layout.cart.items_count' | t: count: cart.item_count }}</span></div>{% endif %}</div></a>\")
+                });
+              }
+            }
+          });
+        }
+      }
+
+      start();
+
+      if (typeof window.appton !== 'undefined'){
+        console.log(\"Jquery version==>\",jQuery.fn.jquery);
+          loadScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js', function() {
+            jQuery340 = jQuery.noConflict(true);
+            goCartStart(jQuery340);
+          });
+      }else{
+        loadScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js', function() {
+          jQuery340 = jQuery.noConflict(true);
+          goCartStart(jQuery340);
         });
-        
+      }
       </script>
       <style>
         .site-header__cart-count {
