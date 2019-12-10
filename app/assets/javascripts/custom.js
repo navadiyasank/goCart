fe268c1e35d51f8253fed2749bdea874bab7cb08
sanket navadiya <sanknavadiya162@gmail.com>
2019-12-10 18:56:30 +0530

    no cart item preview added

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,4 @@
-var timer;
+var timer,no_cart_timer;
 $("body").on("change",".Polaris-Checkbox__Input",function(){
 	$(this).toggleClass('Polaris-Checkbox__Input--checked');
 });
@@ -69,25 +69,38 @@ $(document).on('change', 'input[name="shop[title_animation_type]"]', function(ev
 });
 
 function change_animation(){
-  page_title = $('#shop_cart_title_text').val();
-  page_title = page_title.replace('{cart_items}', '2');
-  defaultTitle = '&nbsp;&nbsp;';position = 0;
+  page_cart_title = $('#shop_cart_title_text').val();
+  page_cart_title = page_cart_title.replace('{cart_items}', '2');
+  page_no_cart_title = $('#shop_no_cart_title_text').val();
+  page_no_cart_title = page_no_cart_title.replace('{cart_items}', '2');
+  defaultTitle = '&nbsp;&nbsp;';position = 0;no_cart_position = 0;
   title_animation_type = $("input[name='shop[title_animation_type]']:checked").val();
-  title = $('.advance-preview');
+  cart_title = $('.advance-preview-with-cart');
+  no_cart_title = $('.advance-preview-no-cart');
   clearInterval(timer);
+  clearInterval(no_cart_timer);
   if(title_animation_type == 'blink'){
     timer = setInterval(function() {
-      if(title.html() == defaultTitle)
-        title.html(page_title);
-      else
-        title.html(defaultTitle);
+      if(cart_title.html() == defaultTitle){
+        cart_title.html(page_cart_title);
+        no_cart_title.html(page_no_cart_title);
+      }
+      else{
+        cart_title.html(defaultTitle);
+        no_cart_title.html(defaultTitle);
+      }
     }, 1000);
   }
   else if(title_animation_type == 'scroll'){
     timer = setInterval(function() {
-      title.text(page_title.substring(position, page_title.length) + page_title.substring(0, position)); 
+      cart_title.text(page_cart_title.substring(position, page_cart_title.length) + page_cart_title.substring(0, position)); 
       position = position + 2;
-      if (position > page_title.length) position = 0
-    }, 1000)
+      if (position > page_cart_title.length) position = 0
+    }, 1000);
+    no_cart_timer = setInterval(function() {
+      no_cart_title.text(page_no_cart_title.substring(no_cart_position, page_no_cart_title.length) + page_no_cart_title.substring(0, no_cart_position)); 
+      no_cart_position = no_cart_position + 2;
+      if (no_cart_position > page_no_cart_title.length) no_cart_position = 0
+    }, 1000);
   }
 }
--- db/migrate/20191130155322_add_title_text_and_title_animation_type_to_shop.rb
+++ db/migrate/20191130155322_add_title_text_and_title_animation_type_to_shop.rb
@@ -1,6 +1,6 @@
 class AddTitleTextAndTitleAnimationTypeToShop < ActiveRecord::Migration[5.2]
   def change
-    add_column :shops, :cart_title_text, :string,default: "{cart_items} items missing"
+    add_column :shops, :cart_title_text, :string,default: "💥 You have {cart_items} items in your cart."
     add_column :shops, :no_cart_title_text, :string,default: "⚡️ Secret Sale ⚡️ Save 20% Off"
     add_column :shops, :title_animation_type, :string,default: "blink"
     add_column :shops, :is_paid, :boolean
